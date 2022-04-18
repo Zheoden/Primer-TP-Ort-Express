30a577d4837645a3b4db0ca89c68b8ed899547a9
Zheoden <schifferjulian@gmail.com>
2022-04-18 00:44:17 -0300

    esqueleto de pizza API

--- src/controllers/pizzaController.js
+++ src/controllers/pizzaController.js
@@ -1,18 +1,50 @@
 import { Router } from 'express';
+import { PizzaService } from '../services/pizzaService.js';
 
 const router = Router();
+const pizzaService = new PizzaService();
 
 router.get('', async (req, res) => {
-    console.log(`This is a get operation`);
+  console.log(`This is a get operation`);
 
-    return res.status(200).json();
+  const pizzas = pizzaService.getPizza();
+
+  return res.status(200).json(pizzas);
+});
+
+router.get('/:id', async (req, res) => {
+  console.log(`Request URL Param: ${req.params.id}`);
+  console.log(`This is a get operation`);
+
+  const pizza = pizzaService.getPizzaById(req.params.id);
+
+  return res.status(200).json(pizza);
 });
 
 router.post('', async (req, res) => {
-    console.log(`Request Data: ${req.body}`)
-    console.log(`This is a post operation`);
+  console.log(`This is a post operation`);
 
-    return res.status(200).json();
+  const pizza = pizzaService.createPizza(req.body);
+
+  return res.status(201).json(pizza);
+});
+
+router.put('/:id', async (req, res) => {
+  console.log(`Request URL Param: ${req.params.id}`);
+  console.log(`This is a put operation`);
+
+  const pizza = pizzaService.updatePizzaById(req.body);
+
+  return res.status(200).json(pizza);
+});
+
+router.delete('/:id', async (req, res) => {
+  console.log(`Request URL Param: ${req.params.id}`);
+  console.log(`This is a delete operation`);
+
+  const pizza = pizzaService.deletePizzaById(req.params.id);
+
+  return res.status(200).json(pizza);
 });
 
 export default router;
---
+++ src/services/pizzaService.js
@@ -0,0 +1,21 @@
+export class PizzaService {
+    getPizza = () => {
+        console.log('This is a function on the service');
+    }
+
+    getPizzaById = () => {
+        console.log('This is a function on the service');
+    }
+
+    createPizza = () => {
+        console.log('This is a function on the service');
+    }
+
+    updatePizzaById = () => {
+        console.log('This is a function on the service');
+    }
+
+    deletePizzaById = () => {
+        console.log('This is a function on the service');
+    }
+}
