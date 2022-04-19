c1f00242ac18d98533f696c5d0e8bd6b589287b9
Zheoden <schifferjulian@gmail.com>
2022-04-19 18:18:35 -0300

    Agrego conexion a base de datos

--- src/services/pizzaService.js
+++ src/services/pizzaService.js
@@ -1,21 +1,61 @@
+import sql from 'mssql'
+import config from '../../db.js'
+
+const pool = await sql.connect(config);
 export class PizzaService {
+
     getPizza = () => {
         console.log('This is a function on the service');
+        const response = await pool.Request().query('SELECT * from [dbo].[Pizza]');
+        console.log(response)
+
+        return response;
     }
 
-    getPizzaById = () => {
+    getPizzaById = (id) => {
         console.log('This is a function on the service');
+        const response = await pool.request()
+            .input('id',sql.Int, id)
+            .query('SELECT * from [dbo].[Pizza] where id = @id');
+        console.log(response)
+
+        return response;
     }
 
-    createPizza = () => {
+    createPizza = (pizza) => {
         console.log('This is a function on the service');
+        const response = await pool.request()
+            .input('Nombre',sql.NChar, pizza?.nombre ?? '')
+            .input('LibreGluten',sql.Bit, pizza?.libreGluten ?? false)
+            .input('Importe',sql.NChar, pizza?.importe ?? 0)
+            .input('Descripcion',sql.NChar, pizza?.description ?? '')
+            .query('INSERT INTO [dbo].[Pizza](Nombre, LibreGluten, Importe, Descripcion) VALUES (@Nombre, @LibreGluten, @Importe, @Descripcion)');
+        console.log(response)
+
+        return response;
     }
 
-    updatePizzaById = () => {
+    updatePizzaById = (id, pizza) => {
         console.log('This is a function on the service');
+        const response = await pool.request()
+            .input('id',sql.Int, id)
+            .input('Nombre',sql.NChar, pizza?.nombre ?? '')
+            .input('LibreGluten',sql.Bit, pizza?.libreGluten ?? false)
+            .input('Importe',sql.NChar, pizza?.importe ?? 0)
+            .input('Descripcion',sql.NChar, pizza?.description ?? '')
+            .query('UPDATE Pizzas SET Nombre = @Nombre, LibreGluten = @LibreGluten, Importe = @Importe, Descripcion = @Descripcion WHERE id = @Id');
+        console.log(response)
+
+        return response;
     }
 
-    deletePizzaById = () => {
+    deletePizzaById = (id) => {
         console.log('This is a function on the service');
+        const response = await pool.request()
+            .input('id',sql.Int, id)
+            .query('DELETE FROM [dbo].[Pizza] WHERE id = @id');
+        console.log(response)
+
+        return response;
     }
 }
